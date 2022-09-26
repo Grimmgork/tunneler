5d66a2f21b1c5e5b64415ce7fe0eee2605e48aa2
Eric <mmmru7shj@gmail.com>
2022-09-26 16:31:15 +0200

    working ...

--- tunneler.pl
+++ tunneler.pl
@@ -17,7 +17,9 @@ while(1) {
 	}
 
 	my ($hostname, $port) = filename_to_host($filename);
+	print "# HOST: $hostname:$port\n";
 	write_host(traverse_host($hostname, $port), $filename);
+	print "HOST DONE!";
 }
 
 sub pick_unvisited_host{
@@ -25,7 +27,6 @@ sub pick_unvisited_host{
 	opendir(DIR, $dirname) or die "Could not open $dirname\n";
 
 	my $result;
-
 	my $filename;
 	while ($filename = readdir(DIR)) {
   		next unless -f $filename;
@@ -67,39 +68,64 @@ sub write_host{
 	my ($host, $filename) = @_;
 
 	open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
-	print $fh "Host!\n";
+	printf $fh "%s\n", $host->name;
+	printf $fh "%s\n", $host->port;
+	print $fh "#REF\n";
+	if(@{$host->refs} > 0){
+		print $fh join("\n", @{$host->refs});
+		print $fh "\n";
+	}
+	print $fh "#STRUCT";
+	if(@{$host->endpoints} > 0){
+		print $fh "\n";
+		print $fh join("\n", @{$host->endpoints});
+	}
 	close $fh;
-	print "written host!";
 }
 
 sub traverse_host{
 	my ($hostname, $port) = @_;
-	print "# $hostname:$port\n";
 
 	my $host=Host->new();
 	$host->name($hostname);
 	$host->port($port);
 
-	traverse_gopher_page($host, ""); # recursively traverse all gopher pages of server, starting with the index page (empty path).
+	traverse_gopher_page_recursively($host, "/"); # recursively traverse all gopher pages of server, starting with the index page (empty path).
 	
 	return $host;
 }
 
-sub traverse_gopher_page{
+sub trim{
+	my($str) = @_;
+	$str =~ s/^\s+|\s+$//g;
+	return $str;
+}
+
+sub rm_trailing_slash{
+	my($str) = @_;
+	$str =~ s{/$}{};
+	return $str;
+}
+
+sub clean_path{
+	my ($path) = @_;
+	unless(defined $path){
+		return "/";
+	}
+	$path = trim($path);
+	if($path eq "" || $path eq "/"){
+		return "/";
+	}
+	return rm_trailing_slash($path);
+}
+
+sub traverse_gopher_page_recursively{
 	my($host, $path) = @_;
 
-	unless(defined $path){
-		$path = "/";
-	}
+	$path = clean_path($path);
 
-	$path =~ s/^\s+|\s+$//g; #trim the ends
-
-	if($path eq ""){
-		$path = "/";
-	}
-
-	sleep(1);
-	print "$path\n";
+	push(@{$host->endpoints}, "1$path");
+	print "+ GOPHER: 1$path\n";
 
 	# request gopher page
 	my @rows = request_gopher($host->name, $host->port, $path);
@@ -108,28 +134,44 @@ sub traverse_gopher_page{
 	foreach my $row (@rows) {
 		# register unknown hosts
 		my ($rowtype, $rowinfo, $rowpath, $rowhost, $rowport) = $row =~ m/^(.)([^\t]*)?\t?([^\t]*)?\t?([^\t]*)?\t?([^\t]\d*)?/;
-		# map file endpoints
+		$rowpath = clean_path($rowpath);
+		$rowhost = trim($rowhost);
+		$rowport = trim($rowport);
 
-		unless($rowtype == "i" || $rowtype == "3"){
-			if(($rowhost eq $host->name) && ($rowport == $host->port)){
-				# link is on the current host
-				# register endpoint
-				my $pathref = "$rowtype$rowpath";
-				unless( grep(/^$pathref$/, @{$host->endpoints})) {
+		if($rowtype eq "i" || $rowtype eq "3" || $rowtype eq ""){
+			next;
+		}
+
+		unless((defined $rowhost) && (defined $rowport)){
+			next;
+		}
+
+		if(($rowhost eq "") && ($rowport eq "")){
+			next;
+		}
+
+		if(($rowhost eq $host->name) && ($rowport eq $host->port)){
+			# link is on the current host
+			# register endpoint
+			my $pathref = "$rowtype$rowpath";
+			unless( grep(/^\Q$pathref\E$/, @{$host->endpoints}) ) {
+				if($rowtype eq "1"){
+					traverse_gopher_page_recursively($host, $rowpath);
+				}
+				else{
 					push(@{$host->endpoints}, $pathref);
-					if($rowtype == "1"){					
-						traverse_gopher_page($host, $rowpath);
-					}
+					print "$pathref\n";
 				}
 			}
-			else
-			{
-				# link to another host
-				# register host if unknown
-				unless(is_host_registered($rowhost, $rowport)){
-					print "found another host: $rowhost $rowport\n";
-					register_unvisited_host($rowhost, $rowport);
-				}
+		}
+		else
+		{
+			# link to another host
+			# register host if unknown
+			unless(is_host_registered($rowhost, $rowport)){
+				register_unvisited_host($rowhost, $rowport);
+				push(@{$host->refs}, "$rowhost:$rowport");
+				print "* found new host: $rowhost:$rowport\n";
 			}
 		}
 	}
@@ -145,7 +187,5 @@ sub request_gopher{
 	);
 
 	$socket->send("$path\n");
-	# my $content = join('',<$socket>);
-
 	return <$socket>;
 }
