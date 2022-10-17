f35d37aa445a05cde3e70dad27f0506535290a1b
Eric <mmmru7shj@gmail.com>
2022-10-17 02:30:12 +0200

    add better endpoint data-structure

--- tunneler.pl
+++ tunneler.pl
@@ -3,6 +3,7 @@ use Class::Struct;
 use IO::Socket::INET;
 use IO::Select;
 use Net::Ping;
+use List::Util 'first';
 
 require './data.pl';
 
@@ -13,12 +14,34 @@ struct Host => {
 	endpoints => '%'
 };
 
+struct PathNode => {
+	parent => '$',
+	name => '$',
+	gophertype => '$',
+	childs => '@'
+};
+
+#my $root=PathNode->new();
+#$root->gophertype('1');
+
+#for((1..1000)){
+#	my $path = join "/", "/test/kajdkjawkd/adkajwdkjawkdj", rand(0xffffffff);
+#	print try_add_path_to_endpoints($root, $path), "\n";
+#}
+
+#try_add_path_to_endpoints($root, "/kek/lel/kok.");
+#try_add_path_to_endpoints($root, "/kek/lel/k");
+
+#print get_full_endpoint_path($leave, ());
+
+#exit();
+
 my $PING = "8.8.8.8";
 print "INDEXING ...\n";
 my $DBH = data_connect("gopherspace.db");
 print "DONE!\n";
 
-# prompt user for a host if no unvisited host is known
+# prompt user for a host if no unvisited host is in the database
 unless(data_get_first_host_unvisited($DBH)){
 	my $inpt = defined $ARGV[0] ? $ARGV[0] : prompt("host:port?");
 	my ($host, $port) = split_host_port($inpt);
@@ -49,6 +72,57 @@ sub try_register_host{
 	return data_register_new_host($DBH, $hostname, $port);
 }
 
+sub try_add_path_to_endpoints{
+	my ($root, $path, $type) = @_;
+	my @segments = split(/\//, $path);
+	shift @segments;
+	my $current = $root;
+	my $isnew = undef;
+	foreach my $segment ( @segments ) {
+		# search children
+		my $found = undef;
+		foreach (@{$current->childs}){
+			if($_->name eq $segment){
+				$found = $_;
+				last;
+			}
+		}
+		unless(defined $found){
+			# add new segment-node
+			$isnew = 1;
+			$found = PathNode->new();
+			$found->name($segment);
+			$found->parent($current);
+			$found->gophertype("0");
+			push @{$current->childs}, $found;
+		}
+
+		$current = $found;
+		# set $current to the found/new segment-node
+	}
+
+	if(defined $isnew){
+		return $current;
+	}
+
+	return undef; #if it is already in the structure
+}
+
+sub get_next_unvisited_endpoint_depth_first{
+	my ($root, $node) = @_;
+
+	return;
+}
+
+sub get_full_endpoint_path{
+	my ($node, @segments) = @_;
+	unshift(@segments, $node->name);
+	unless(defined $node->parent){
+		return join("/", @segments);
+	}
+	return get_full_endpoint_path($node->parent, @segments);
+}
+
 sub traverse_host{
 	my ($hostname, $port) = @_;
 
@@ -58,7 +132,7 @@ sub traverse_host{
 
 	data_load_endpoint_cache_for_host($DBH, $host, $port); # prepare database for host
 
-	my $err = traverse_gopher_page_recursively($host, "", 3); # recursively traverse gopher menus of host, starting with the index page "/".
+	my $err = traverse_gopher_page_recursively($host, "", 5); # recursively traverse gopher menus of host, starting with the index page "/".
 	if(defined $err){
 		data_set_host_status($DBH, $hostname, $port, 1); # traversed
 	}else{
@@ -83,26 +157,15 @@ sub trim{
 	return $str;
 }
 
-sub rm_trailing_slash{
-	my($str) = @_;
-	$str =~ s{/$}{};
-	return $str;
-}
-
 sub clean_path{
 	my ($path) = @_;
-	$path = trim($path);
-	if($path eq "" || $path eq "/"){
-		return "/";
+	$path =~ s/\\/\//g; #replace \ with /
+	$path =~ s/^[\s\/]+|[\/\s]+$//g; #trim right / and left + right withespaces
+	if($path eq ""){
+		return "";
 	}
-	unless(defined $path){
-		return "/";
-	}
-	unless($path =~ /^\//){
-		$path = "/$path";
-	}
-	$path =~ s/\\/\//g;
-	return rm_trailing_slash($path);
+	$path = "/$path";
+	return $path;
 }
 
 sub has_internet_connection{
@@ -125,8 +188,7 @@ sub traverse_gopher_page_recursively{
 
 	# dont wander into deep, dark caverns ... o~o
 	if($depth < 0 || $path =~ /(\/commit\/|archive|.git)/gi){ 
-		printf " -> SKIP: %s:%s 1%s\n", $host->name, $host->port, $path;
-		return 1; # self restriction error
+		return 2; # self restriction error
 	}
 
 	$path = clean_path($path);
@@ -140,13 +202,10 @@ sub traverse_gopher_page_recursively{
 		@rows = request_gopher($host->name, $host->port, $path);
 	};
 	if ($@) {
-		print " X ERROR: $@ \n";
-		data_set_endpoint_status($DBH, $endpoint_id, 3);
-		return 2; # external error
+		return 3; # external error
 	}
 
-	data_set_endpoint_status($DBH, $endpoint_id, 1);
-	printf " -> MAP: %s:%s 1%s\n", $host->name, $host->port, $path;
+	printf " -> ENTER: %s:%s 1%s\n", $host->name, $host->port, $path;
 
 	# iterate rows
 	foreach my $row (@rows) {
@@ -157,7 +216,8 @@ sub traverse_gopher_page_recursively{
 			next;
 		}
 
-		if(my ($url) = $rowpath =~ m/^UR[LI]:(.*)/gi){ # extract a full url reference like: URL:http://example.com
+		if(my ($url) = $rowpath =~ m/^UR[LI]:(.*)/gi)
+		{ # extract a full url reference like: URL:http://example.com
 			my($protocol, $hostname) = $url =~ m/^([a-z0-9]*):\/\/([^\/:]*)/gi;
 			unless(defined $protocol){
 				next;
@@ -170,43 +230,62 @@ sub traverse_gopher_page_recursively{
 		$rowpath = clean_path($rowpath);
 		$rowhost = trim(lc $rowhost); # lowercase and trim the domain name
 
-		if(($rowhost eq "") or not defined $rowhost){
+		# exclude invalid host names
+		if($rowhost =~ /[^a-z0-9-\.]/i){
 			next;
 		}
 
-		if(($rowport eq "") or not defined $rowport){
+		# exclude *.onion and ftp.* domains
+		if($rowhost =~ /(^ftp\.|\.onion$)/gi){
 			next;
 		}
 
-		# link to foreign host
-		unless(($rowhost eq $host->name) && ($rowport eq $host->port)){
-			if($rowtype =~ /^[^8T+2]$/){
-				# exclude *.onion and ftp.* domains
-				if($rowhost =~ /(^ftp\.|\.onion$)/gi){
-					next;
+		if($rowhost eq ""){
+			$rowhost = $host->name;
+		}
+
+		if($rowport eq ""){
+			$rowport = $host->port;
+		}
+
+		if(($rowhost eq $host->name) && ($rowport eq $host->port))
+		{ # endpoint of current host
+			unless(defined data_get_endpoint_id($rowpath)) {
+				print "$rowtype$rowpath\n";
+				if($rowtype eq "1"){
+					# traverse gopher page and hendle the error
+					my $err = traverse_gopher_page_recursively($host, $rowpath, $depth-1);
+					if($err != 0)
+					{ # error occured while traversal
+						if($err == 2) { printf " -> SKIP: %s:%s 1%s\n", $host->name, $host->port, $path; }
+						elsif ($err == 3) { print " X EXTERNAL ERROR: $@ \n"; }
+						else { print " ? UNKNOWN ERROR\n"; }
+					}
+					else{
+						# successful traversal
+						printf " -> DONE: %s:%s 1%s\n", $host->name, $host->port, $path;
+					}
+					data_set_endpoint_status($DBH, $endpoint_id, 1 + $err);
 				}
-
-				# make sure host is registered
-				if(defined try_register_host($rowhost, $rowport)){ 
-					print " * DICOVERED: $rowhost:$rowport\n";
+				else{
+					data_try_add_endpoint($DBH, $rowhost, $rowport, $rowtype, $rowpath, 0);
 				}
-				data_increment_reference($DBH, $host->name, $host->port, $rowhost, $rowport);
-				print " ~ REF: $rowhost:$rowport\n";
 			}
-			next;
 		}
-
-		# register local host endpoint
-		my $pathref = "$rowtype$rowpath";
-		unless(data_get_endpoint_id($rowpath)) {
-			print "$pathref\n";
-			if($rowtype eq "1"){
-				traverse_gopher_page_recursively($host, $rowpath, $depth-1)
-			}else{
-				data_try_add_endpoint($DBH, $rowhost, $rowport, $rowtype, $rowpath, 0);
+		else
+		{
+			# link to foreign host
+			# make sure host is registered
+			if(defined try_register_host($rowhost, $rowport)){ 
+				print " * DICOVERED: $rowhost:$rowport\n";
 			}
+
+			data_increment_reference($DBH, $host->name, $host->port, $rowhost, $rowport);
+			print " ~ REF: $rowhost:$rowport\n";
 		}
 	}
+
+	return 0;
 }
 
 sub request_gopher{
